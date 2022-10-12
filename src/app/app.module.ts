0e833b4ec61c440a3abb2b721f17cc1a05c09839
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-12 09:37:15 +0530

    hgv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LinkdataComponent } from './linkdata/linkdata.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const link:Routes=[
+  {path:"",component:LinkdataComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LinkdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(link)
   ],
   providers: [],
   bootstrap: [AppComponent]
